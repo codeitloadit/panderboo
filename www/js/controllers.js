1568d56a69bc78042bb3c76d6a2ecc6f14a7b8d2
codeitloadit <brian@codeitload.it>
2015-06-06 20:23:44 -0700

    Add support for panderboo friends

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,25 +16,34 @@ angular.module('panderboo.controllers', ['firebase'])
 
         $rootScope.authData = Auth.$getAuth();
 
-        $scope.raw_friends = [];
-        $scope.friends = [];
+        $scope.rawPanderbooFriends = [];
+        $scope.rawInvitableFriends = [];
+        $scope.panderbooFriends = [];
+        $scope.invitableFriends = [];
         $scope.phrase = '';
 
         $scope.filterByPhrase = function (phrase) {
-            $scope.friends = [];
-            angular.forEach($scope.raw_friends, function(friend) {
+            $scope.panderbooFriends = [];
+            $scope.invitableFriends = [];
+            angular.forEach($scope.rawPanderbooFriends, function (friend) {
                 if (friend.last_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || friend.first_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || (friend.middle_name && friend.middle_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0)) {
-                    $scope.friends.push(friend);
+                    $scope.panderbooFriends.push(friend);
+                }
+            });
+            angular.forEach($scope.rawInvitableFriends, function (friend) {
+                if (friend.last_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || friend.first_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || (friend.middle_name && friend.middle_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0)) {
+                    $scope.invitableFriends.push(friend);
                 }
             });
         };
 
         if ($rootScope.authData) {
-            $scope.url = 'https://graph.facebook.com/v2.3/me/invitable_friends?fields=id,picture,first_name,last_name,middle_name&limit=9999&access_token=' + $rootScope.authData.facebook.accessToken;
-            $http.get($scope.url)
+            $scope.panderbooFriendsUrl = 'https://graph.facebook.com/v2.3/me?fields=friends.limit(9999){first_name,middle_name,last_name,picture}&limit=9999&access_token=' + $rootScope.authData.facebook.accessToken;
+            $scope.invitableFriendsUrl = 'https://graph.facebook.com/v2.3/me/invitable_friends?fields=id,picture,first_name,last_name,middle_name&limit=9999&access_token=' + $rootScope.authData.facebook.accessToken;
+            $http.get($scope.panderbooFriendsUrl)
                 .then(function (response) {
-                    $scope.raw_friends = response.data.data;
-                    $scope.raw_friends.sort(function compare(a,b) {
+                    $scope.rawPanderbooFriends = response.data.friends.data;
+                    $scope.rawPanderbooFriends.sort(function compare(a, b) {
                         if (a.last_name == b.last_name) {
                             if (a.first_name < b.first_name)
                                 return -1;
@@ -47,7 +56,25 @@ angular.module('panderboo.controllers', ['firebase'])
                             return 1;
                         return 0;
                     });
-                    $scope.friends = $scope.raw_friends;
+                    $scope.panderbooFriends = $scope.rawPanderbooFriends;
+                });
+            $http.get($scope.invitableFriendsUrl)
+                .then(function (response) {
+                    $scope.rawInvitableFriends = response.data.data;
+                    $scope.rawInvitableFriends.sort(function compare(a, b) {
+                        if (a.last_name == b.last_name) {
+                            if (a.first_name < b.first_name)
+                                return -1;
+                            if (a.first_name > b.first_name)
+                                return 1;
+                        }
+                        if (a.last_name < b.last_name)
+                            return -1;
+                        if (a.last_name > b.last_name)
+                            return 1;
+                        return 0;
+                    });
+                    $scope.invitableFriends = $scope.rawInvitableFriends;
                 })
                 .finally(function () {
                     $ionicLoading.hide();
@@ -84,7 +111,7 @@ angular.module('panderboo.controllers', ['firebase'])
                     $scope.errors.push(error.toString());
                 });
             } else {
-                Auth.$authWithOAuthPopup('facebook', function(error, authData) {
+                Auth.$authWithOAuthPopup('facebook', function (error, authData) {
                     if (error) {
                         $scope.errors.push(error.toString());
                     } else {
