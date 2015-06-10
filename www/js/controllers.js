307ecdb96e3b8575e40f8f4993112ecb500f17c3
codeitloadit <brian@codeitload.it>
2015-06-09 23:29:40 -0700

    Move friends logic to factories

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,82 +12,45 @@ angular.module('panderboo.controllers', ['firebase'])
 
     })
 
-    .controller('FriendsCtrl', function ($scope, $http, AuthData, $ionicLoading, $window) {
-        $ionicLoading.show({
-            template: 'Loading...'
-        });
-
+    .controller('FriendsCtrl', function ($scope, $ionicLoading, PanderbooFriends, InvitableFriends) {
         $scope.refresh = function () {
-            $window.location.reload(true)
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+            $scope.phrase = '';
+            PanderbooFriends.fetchFriends(function (friends) {
+                $scope.panderbooFriends = friends;
+                InvitableFriends.fetchFriends(function (friends) {
+                    $scope.invitableFriends = friends;
+                    $scope.$broadcast('scroll.refreshComplete');
+                    $ionicLoading.hide();
+                });
+            });
         };
-
-        $scope.rawPanderbooFriends = [];
-        $scope.rawInvitableFriends = [];
-        $scope.panderbooFriends = [];
-        $scope.invitableFriends = [];
-        $scope.phrase = '';
+        $scope.refresh();
 
         $scope.filterByPhrase = function (phrase) {
             $scope.panderbooFriends = [];
             $scope.invitableFriends = [];
-            angular.forEach($scope.rawPanderbooFriends, function (friend) {
+            angular.forEach(PanderbooFriends.getFriends(), function (friend) {
                 if (friend.last_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || friend.first_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || (friend.middle_name && friend.middle_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0)) {
                     $scope.panderbooFriends.push(friend);
                 }
             });
-            angular.forEach($scope.rawInvitableFriends, function (friend) {
+            angular.forEach(InvitableFriends.getFriends(), function (friend) {
                 if (friend.last_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || friend.first_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0 || (friend.middle_name && friend.middle_name.toLowerCase().indexOf(phrase.toLowerCase()) >= 0)) {
                     $scope.invitableFriends.push(friend);
                 }
             });
         };
 
-        if (AuthData.get()) {
-            $scope.panderbooFriendsUrl = 'https://graph.facebook.com/v2.3/me?fields=friends.limit(9999){first_name,middle_name,last_name,picture}&limit=9999&access_token=' + AuthData.accessToken();
-            $scope.invitableFriendsUrl = 'https://graph.facebook.com/v2.3/me/invitable_friends?fields=id,picture,first_name,last_name,middle_name&limit=9999&access_token=' + AuthData.accessToken();
-            $http.get($scope.panderbooFriendsUrl)
-                .then(function (response) {
-                    $scope.rawPanderbooFriends = response.data.friends.data;
-                    $scope.rawPanderbooFriends.sort(function compare(a, b) {
-                        if (a.last_name == b.last_name) {
-                            if (a.first_name < b.first_name)
-                                return -1;
-                            if (a.first_name > b.first_name)
-                                return 1;
-                        }
-                        if (a.last_name < b.last_name)
-                            return -1;
-                        if (a.last_name > b.last_name)
-                            return 1;
-                        return 0;
-                    });
-                    $scope.panderbooFriends = $scope.rawPanderbooFriends;
-                });
-            $http.get($scope.invitableFriendsUrl)
-                .then(function (response) {
-                    $scope.rawInvitableFriends = response.data.data;
-                    $scope.rawInvitableFriends.sort(function compare(a, b) {
-                        if (a.last_name == b.last_name) {
-                            if (a.first_name < b.first_name)
-                                return -1;
-                            if (a.first_name > b.first_name)
-                                return 1;
-                        }
-                        if (a.last_name < b.last_name)
-                            return -1;
-                        if (a.last_name > b.last_name)
-                            return 1;
-                        return 0;
-                    });
-                    $scope.invitableFriends = $scope.rawInvitableFriends;
-                })
-                .finally(function () {
-                    $ionicLoading.hide();
-                });
+        $scope.loadFriend = function (friendId) {
+            console.log(friendId);
         }
     })
 
-    .controller('FriendDetailCtrl', function ($scope) {
+    .controller('FriendDetailCtrl', function ($scope, AuthData) {
+
     })
 
     .controller('SettingsCtrl', function ($scope, $state, AuthData) {
--- www/js/services.js
+++ www/js/services.js
@@ -51,4 +51,80 @@ angular.module('panderboo.services', [])
 
             return factory;
         }
+    ])
+
+    .factory('PanderbooFriends', ['AuthData', '$http',
+        function (AuthData, $http) {
+            var friends = [];
+            var factory = {};
+
+            factory.fetchFriends = function (callback) {
+                var panderbooFriendsUrl = 'https://graph.facebook.com/v2.3/me?fields=friends.limit(9999){first_name,middle_name,last_name,picture}&limit=9999&access_token=' + AuthData.accessToken();
+                $http.get(panderbooFriendsUrl)
+                    .then(function (response) {
+                        friends = response.data.friends.data;
+                        friends.sort(function compare(a, b) {
+                            if (a.last_name == b.last_name) {
+                                if (a.first_name < b.first_name)
+                                    return -1;
+                                if (a.first_name > b.first_name)
+                                    return 1;
+                            }
+                            if (a.last_name < b.last_name)
+                                return -1;
+                            if (a.last_name > b.last_name)
+                                return 1;
+                            return 0;
+                        });
+                        return callback(friends);
+                    });
+            };
+
+            factory.getFriends = function () {
+                if (!friends) {
+                    return factory.fetchFriends();
+                }
+                return friends;
+            };
+
+            return factory;
+        }
+    ])
+
+    .factory('InvitableFriends', ['AuthData', '$http',
+        function (AuthData, $http) {
+            var friends = [];
+            var factory = {};
+
+            factory.fetchFriends = function (callback) {
+                var invitableFriendsUrl = 'https://graph.facebook.com/v2.3/me/invitable_friends?fields=id,picture,first_name,last_name,middle_name&limit=9999&access_token=' + AuthData.accessToken();
+                $http.get(invitableFriendsUrl)
+                    .then(function (response) {
+                        friends = response.data.data;
+                        friends.sort(function compare(a, b) {
+                            if (a.last_name == b.last_name) {
+                                if (a.first_name < b.first_name)
+                                    return -1;
+                                if (a.first_name > b.first_name)
+                                    return 1;
+                            }
+                            if (a.last_name < b.last_name)
+                                return -1;
+                            if (a.last_name > b.last_name)
+                                return 1;
+                            return 0;
+                        });
+                        return callback(friends);
+                    });
+            };
+
+            factory.getFriends = function () {
+                if (!friends) {
+                    return factory.fetchFriends();
+                }
+                return friends;
+            };
+
+            return factory;
+        }
     ]);
