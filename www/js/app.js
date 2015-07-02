5e11e2e3578132aeafa4853d19b9dde18e41887c
codeitloadit <brian@codeitload.it>
2015-07-01 22:00:53 -0700

    Saving changes before firebase refactor

--- www/js/app.js
+++ www/js/app.js
@@ -134,4 +134,16 @@ angular.module('panderboo', ['ionic', 'panderboo.controllers', 'panderboo.servic
                 }
             });
         };
+    })
+
+    .directive('focusInput', function($timeout) {
+        return {
+            link: function(scope, element, attrs) {
+                element.bind('click', function() {
+                    $timeout(function() {
+                        element.parent().find('input')[0].focus();
+                    });
+                });
+            }
+        };
     });
